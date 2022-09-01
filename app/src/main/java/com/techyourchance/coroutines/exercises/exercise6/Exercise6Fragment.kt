dc1d5c506ee4163150288ff62238caa1bf18aa8b
Natasha Valentina <natasha.valentina1998@gmail.com>
2022-09-01 23:04:34 +0700

    solution to exercise 6

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise6/Exercise6Fragment.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise6/Exercise6Fragment.kt
@@ -25,8 +25,6 @@ class Exercise6Fragment : BaseFragment() {
     private lateinit var btnStart: Button
     private lateinit var txtRemainingTime: TextView
 
-    private var hasBenchmarkBeenStartedOnce = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         benchmarkUseCase = compositionRoot.exercise6BenchmarkUseCase
@@ -48,13 +46,18 @@ class Exercise6Fragment : BaseFragment() {
             }
 
             coroutineScope.launch {
-                btnStart.isEnabled = false
-                val iterationsCount = benchmarkUseCase.executeBenchmark(benchmarkDurationSeconds)
-                Toast.makeText(requireContext(), "$iterationsCount", Toast.LENGTH_SHORT).show()
-                btnStart.isEnabled = true
+                try {
+                    btnStart.isEnabled = false
+                    val iterationsCount =
+                        benchmarkUseCase.executeBenchmark(benchmarkDurationSeconds)
+                    Toast.makeText(requireContext(), "$iterationsCount", Toast.LENGTH_SHORT).show()
+                    btnStart.isEnabled = true
+                } catch (ex: CancellationException) {
+                    logThreadInfo("coroutines cancelled")
+                    btnStart.isEnabled = true
+                    txtRemainingTime.text = "done!"
+                }
             }
-
-            hasBenchmarkBeenStartedOnce = true
         }
 
         return view
@@ -64,10 +67,6 @@ class Exercise6Fragment : BaseFragment() {
         logThreadInfo("onStop()")
         super.onStop()
         coroutineScope.coroutineContext.cancelChildren()
-        if (hasBenchmarkBeenStartedOnce) {
-            btnStart.isEnabled = true
-            txtRemainingTime.text = "done!"
-        }
     }
 
 
